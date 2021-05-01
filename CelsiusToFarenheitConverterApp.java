ca69111a8405632e69f800fe23e3114157257503
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-05-01 23:21:57 +0100

    changed the variable name from farenheitTemp to farenheitTemperature

--- CelsiusToFarenheitConverterApp.java
+++ CelsiusToFarenheitConverterApp.java
@@ -5,15 +5,15 @@ public class CelsiusToFarenheitConverterApp {
     public static void main (String ... args) {
         
         double userInputInCelsius;
-        double farenheitTemp;
+        double farenheitTemperature;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a degree in Celsius: ");
         userInputInCelsius = input.nextDouble();
         System.out.println();
 
-        farenheitTemp = (9.0/5) * userInputInCelsius + 32;
+        farenheitTemperature = (9.0/5) * userInputInCelsius + 32;
 
-        System.out.printf("%.1f Celsius is %.1f Farenheit%n", userInputInCelsius, farenheitTemp);
+        System.out.printf("%.1f Celsius is %.1f Farenheit%n", userInputInCelsius, farenheitTemperature);
     }
 }
